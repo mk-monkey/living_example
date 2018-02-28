bac8d2667066a8a5041db3057ff85f683ed170cd
南飞龙 <nanfeilong@nanfeilongdeMacBook-Pro.local>
2018-02-28 22:52:12 +0800

    备份文件

---
+++ src/main/java/com/living/example/sendmail/contrller/Demo.java
@@ -0,0 +1,15 @@
+package com.living.example.sendmail.contrller;
+
+import java.util.Properties;
+
+/**
+ * @Class Name: Demo
+ * @Description: TODO
+ * @Company bgy:  MK monkey
+ * @create: 2018-02-16 22:34
+ **/
+public class Demo {
+    public static void main(String[] args) {Properties prop = new Properties();
+
+    }
+}
